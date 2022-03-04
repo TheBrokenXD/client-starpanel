e2016659ab75dec0ad5ed3458caf3ea86c000087
Broken <broken.contact.1211@gmail.com>
2022-03-04 14:47:43 +0530

    user dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,33 +1,33 @@
-import Head from 'next/head'
-// firebase
-import useAuth from '../context/AuthContext';
+// import Head from 'next/head'
+// // firebase
+// import useAuth from '../context/AuthContext';
 
-const dashboard = () => {
+// const dashboard = () => {
 
-    const user = useAuth()
+//     const user = useAuth()
 
-    return (
-        <>
+//     return (
+//         <>
 
-            <Head>
-                <title>Affiliate - Starpanel</title>
-            </Head>
+//             <Head>
+//                 <title>Affiliate - Starpanel</title>
+//             </Head>
 
-            <div className="container">
-                <div className="row justify-center align-i-center h-screen">
-                    <div className="col-6-xs">
+//             <div className="container">
+//                 <div className="row justify-center align-i-center h-screen">
+//                     <div className="col-6-xs">
 
-                        <div className="card custom-card-bg p-3">
-                            <p className='white'>Username : {user.displayName}</p>
-                            <p className='white'>Email : {user.email}</p>
-                        </div>
+//                         <div className="card custom-card-bg p-3">
+//                             <p className='white'>Username : {user.displayName}</p>
+//                             <p className='white'>Email : {user.email}</p>
+//                         </div>
 
-                    </div>
-                </div>    
-            </div>
+//                     </div>
+//                 </div>    
+//             </div>
 
-        </>
-    );
-}
+//         </>
+//     );
+// }
  
-export default dashboard;
+// export default dashboard;
